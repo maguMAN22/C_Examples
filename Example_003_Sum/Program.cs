aa4a59c9e9ad39ad4f427675ba2ce924562329da
Gleb Filippov <glebanov7991@gmail.com>
2023-02-23 06:27:13 +0300

    Add Example 003 Sum

---
+++ Example_003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Сумма чисел.");
+Console.WriteLine("Введите первое число:");
+int a = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Int32.Parse(Console.ReadLine());
+int res = a + b;
+Console.WriteLine(res);
