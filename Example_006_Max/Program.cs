3ea0594d37e2a485fd228a755e361afd7c67a271
Gleb Filippov <glebanov7991@gmail.com>
2023-02-23 11:54:14 +0300

    Add Example 006 Max

---
+++ Example_006_Max/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int a = 1;
+int b = 42;
+int c = 9;
+int d = 7;
+int e = 23;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine (max);
