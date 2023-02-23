bd88e98db92b35ca06675b9481a0a2a4915bc395
Gleb Filippov <glebanov7991@gmail.com>
2023-02-23 06:46:49 +0300

    Add Example 004 Div

---
+++ Example_004_Div/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Частное чисел.");
+Console.WriteLine("Введите первое число:");
+double a = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+double b = Double.Parse(Console.ReadLine());
+double res = a / b;
+Console.WriteLine(res);
