77a3627eb2c7e829b667fcf5ca76d8b8bf27e1e3
Gleb Filippov <glebanov7991@gmail.com>
2023-02-25 01:38:41 +0300

    Add Dog

---
+++ Example_008_man&dog/Program.cs
@@ -0,0 +1,24 @@
+﻿int Dist = 10000;
+int FFS = 1;
+int SFS = 2;
+int DS = 5;
+int count = 0;
+int friend = 2;
+int time = 0;
+
+while (Dist > 10)
+{
+    if (friend == 1)
+    {
+        time = Dist / (FFS + DS);
+        friend = 2;
+    }
+    else
+    {
+        time = Dist / (SFS + DS);
+        friend = 1;
+    }
+    Dist = Dist - (FFS + SFS) * time;
+    count++;
+}
+Console.WriteLine(count);
